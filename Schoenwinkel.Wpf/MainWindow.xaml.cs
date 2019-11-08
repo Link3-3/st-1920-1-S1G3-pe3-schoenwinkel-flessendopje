d7d01495f9eefb3cb71be67d2e4cb36a0f67d05a
Tim De Gieter <tim.de.gieter@student.howest.be>
2019-11-08 13:56:03 +0100

    Created and added functionality to Klant.cs Class

--- Schoenwinkel.Wpf/MainWindow.xaml.cs
+++ Schoenwinkel.Wpf/MainWindow.xaml.cs
@@ -12,17 +12,51 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Schoenwinkel.lib;
 
 namespace Schoenwinkel.Wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        Klant klant = new Klant();
+        List<Klant> klanten;
+        //enum Geslachten {Undefined = 0, Man = 1, Vrouw = 2} //enum in de klasse gebruiken //enum is reeds publiek gedefiniëerd in de library
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnVoegKlantToe_Click(object sender, RoutedEventArgs e)
+        {
+            string voornaam = txtVoornaam.Text.ToString();
+            string achternaam = txtFamilienaam.Text.ToString();
+            int.TryParse(txtSchoenmaat.Text, out int schoenmaat);
+            Geslachten geslacht;
+            if (rdbGeslachtMan.IsChecked == true)
+            {
+                geslacht = Geslachten.Man;
+            }
+            else if (rdbGeslachtVrouw.IsChecked == true)
+            {
+                geslacht = Geslachten.Vrouw;
+            }
+            else //geen geslacht gesleecteerd
+            {
+                geslacht = Geslachten.Undefined;
+            }
+
+            try
+            {
+                klant.NieuweKlant(voornaam, achternaam, geslacht, schoenmaat );
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Oeps er ging iets fout" + Environment.NewLine + exception.Message);
+                return;
+            }
+            
+
+            MessageBox.Show($"{klant.Voornaam} {klant.Achternaam} heeft een schoenmaat van {klant.Schoenmaat} en is een {klant.Geslacht}/nmet guid/n {klant.GuID}");
         }
     }
 }
---
+++ Schoenwinkel.lib/Klant.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoenwinkel.lib
+{
+    public enum Geslachten { Undefined = 0, Man = 1, Vrouw = 2 }
+    public class Klant
+    {
+        private string voornaam;
+        private string achternaam;
+        private int schoenmaat;
+        private string guID;
+        
+        private Geslachten geslacht;
+
+        public Geslachten Geslacht
+        {
+            get 
+            { 
+                return geslacht; 
+            }
+            set 
+            {
+                if (value == Geslachten.Undefined)
+                    throw new Exception("Kies een geslacht!");
+                else
+                    geslacht = value; 
+            }
+        }
+
+        public string Voornaam
+        {
+            get
+            {
+                return voornaam;
+            }
+            set
+            {
+                if (value != "")
+                    voornaam = value;
+                else
+                    throw new Exception("Geef een voornaam in!");
+            }
+        }
+
+        public string Achternaam
+        {
+            get
+            {
+                return achternaam;
+            }
+            set
+            {
+                if (value != "")
+                    achternaam = value;
+                else
+                    throw new Exception("Geef een achternaam in!");
+            }
+        }
+
+        public int Schoenmaat 
+        {
+            get
+            {
+                return schoenmaat;
+            }
+            set
+            {
+                if (value >= 10 && value < 50) //kleinste schoenmaat is 10, grootste is 50
+                    schoenmaat = value;
+                else
+                    throw new Exception("De ingegeven schoenmaat moet tussen 10 en 50 zijn");
+            }
+        }
+
+        public string GuID
+        {
+            get
+            {
+                return guID;
+            }
+        }
+
+        public void NieuweKlant(string klantVoornaam, string klantAchternaam, Geslachten klantGeslacht, int klantSchoenmaat )
+        {
+            //genereer nieuwe UserID
+            guID = Guid.NewGuid().ToString(); //bewust gekozen voor geen guid set omdat we niet willen dat iemand dit handmatig aanmaakt.
+            Voornaam = klantVoornaam;
+            Achternaam = klantAchternaam;
+            Geslacht = klantGeslacht;
+            Schoenmaat = klantSchoenmaat;
+        }
+    }
+}
