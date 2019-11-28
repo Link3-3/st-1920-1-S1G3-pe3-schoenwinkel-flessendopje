d6c68148aae348dd90e0a1462b5999b0dfbe0650
Tim De Gieter <tim.de.gieter@student.howest.be>
2019-11-28 08:34:05 +0100

    Add final QOL changes Add more descriptions in the code

--- Schoenwinkel.Wpf/MainWindow.xaml.cs
+++ Schoenwinkel.Wpf/MainWindow.xaml.cs
@@ -18,7 +18,6 @@ namespace Schoenwinkel.Wpf
 {
     public partial class MainWindow : Window
     {
-        
         List<Klant> klanten = new List<Klant>();
         List<Aankoop> aankopen = new List<Aankoop>();
         List<string> verkoopGeschiedenis = new List<string>();
@@ -35,6 +34,7 @@ namespace Schoenwinkel.Wpf
         }
         void KlantenToevoegen() //zet dit gerust uit om te testen
         {
+            //testen met vooringevulde correcte waarden zodat we niet duizend keer een nieuwe klant moeten invoegen.
             Klant klant1 = new Klant("Tim", "De Gieter", Geslachten.Man, "45");
             klanten.Add(klant1);
             Klant klant2 = new Klant("Pieter", "Peters", Geslachten.Man, "22");
@@ -86,16 +86,23 @@ namespace Schoenwinkel.Wpf
                 return;
             }
             klanten.Add(klant); //wordt niet meer uitgevoerd indien fout doordat er een return in de catch zit
-            
+
+            ResetInputFields();
             FillKlantenListBox();
             ClearCostumerLabels();
             ClearCostumerStatistics();
             UpdateStoreStatistics();
-            lstKlantenlijst.SelectedIndex = lstKlantenlijst.Items.Count - 1; //poogt om de nieuwe toegevoegde klant direct te selecteren
-
+            lstKlantenlijst.SelectedIndex = lstKlantenlijst.Items.Count - 1; //de nieuwe toegevoegde klant direct selecteren
         }
-
-        void FillKlantenListBox() //vult de klanten listbox met klanten uit de lijst klanten. alfabetisch maken?
+        void ResetInputFields()
+        {
+            rdbGeslachtMan.IsChecked = false;
+            rdbGeslachtVrouw.IsChecked = false;
+            txtFamilienaam.Text = "";
+            txtVoornaam.Text = "";
+            txtSchoenmaat.Text = "";
+        }
+        void FillKlantenListBox() //vult de klanten listbox met klanten uit de lijst klanten.
         {
             lstKlantenlijst.Items.Clear();
             foreach (Klant klant in klanten)
@@ -103,7 +110,6 @@ namespace Schoenwinkel.Wpf
                 lstKlantenlijst.Items.Add(klant.Achternaam + " " + klant.Voornaam);
             }
         }
-
         void ClearCostumerLabels()
         {
             lblFamilienaam.Content = "-";
@@ -113,20 +119,18 @@ namespace Schoenwinkel.Wpf
             lblGeslacht.Content = "-";
             txtNieuweSchoenmaat.Text = "";
         }
-
         void ClearCostumerStatistics()
         {
             lblAantalGekochteSchoenen.Content = "-";
             lblPrijsGekochteSchoenen.Content = "-";
             lstGekochteSchoenen.Items.Clear();
         }
-
         void UpdateCostumerStatistics()
         {
             lstGekochteSchoenen.Items.Clear();
             //find number of articles the client has bought & show price of total items bought
             int index = lstKlantenlijst.SelectedIndex;
-            if (index == -1)
+            if (index == -1) //checks if a client is selected
                 return;
             Klant geselecteerdeKlant = klanten[index];
             string guID = geselecteerdeKlant.GuID;
@@ -145,14 +149,12 @@ namespace Schoenwinkel.Wpf
             lblAantalGekochteSchoenen.Content = numberOfItemsBought.ToString();
             lblPrijsGekochteSchoenen.Content = "€ " + priceOfItemsBought.ToString();
         }
-
         void UpdateStoreStatistics()
         {
             Winkel winkel = new Winkel();
 
             lblAantalVerkochteSchoenen.Content = winkel.AantalVerkochteSchoenen(aankopen);
             lblPrijsVerkochteSchoenen.Content = "€ " + winkel.TotaalPrijsVerkochteSchoenen(aankopen);
-
 
             lblGrootsteSchoenmaat.Content = winkel.GrootsteSchoenmaat(klanten);
             lblKleinsteSchoenmaat.Content = winkel.KleinsteSchoenmaat(klanten);
@@ -164,10 +166,7 @@ namespace Schoenwinkel.Wpf
             {
                 lstVerkochteSchoenen.Items.Add(sale);
             }
-            
-            
         }
-
         private void lstKlantenlijst_SelectionChanged(object sender, SelectionChangedEventArgs e) //vult de labels met de geselecteerde klant.
         {
             int index = lstKlantenlijst.SelectedIndex;
@@ -186,7 +185,6 @@ namespace Schoenwinkel.Wpf
             txtSchoenPrijs.Text = "";
             UpdateCostumerStatistics();
         }
-
         private void btnWijzigSchoenmaat_Click(object sender, RoutedEventArgs e) //schoenmaat van een al bestaande klant wijzigen
         {
             int index = lstKlantenlijst.SelectedIndex;
@@ -213,9 +211,7 @@ namespace Schoenwinkel.Wpf
             {
                 MessageBox.Show("Selecteer eerst een klant alvorens de schoenmaat te wijzigen", "Fout!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            
         }
-
         private void btnVerkoopSchoen_Click(object sender, RoutedEventArgs e) //klant koopt een paar schoenen
         {
             if (lstKlantenlijst.SelectedIndex == -1)
@@ -240,29 +236,21 @@ namespace Schoenwinkel.Wpf
                 }
                 aankopen.Add(aankoop);
 
-
-                foreach (Klant klant in klanten) //wooow zotte dingen hier, misschien niet cpu vriendelijk
+                foreach (Klant klant in klanten)
                 {
                     if (aankoop.GuID == klant.GuID) //dit is de klant die dit item kocht
                     {
                         verkoopGeschiedenis.Insert(0,($"{klant.Achternaam} {klant.Voornaam} - {aankoop.Merk} - {aankoop.Kleur} - € {aankoop.Prijs}"));
                     }
                 }
-                
-
                 UpdateCostumerStatistics();
                 UpdateStoreStatistics();
-                //MessageBox.Show(aankoop.GuID + aankoop.Kleur + aankoop.Merk + aankoop.Prijs); //debug
             }
         }
-
-
 
         private void btnVerwijderKlant_Click(object sender, RoutedEventArgs e)
         {
             //bij het verwijderen van deze klant zullen ook de aangekochte schoenen verwijderd worden
-
-            //messagebox om te vragen of je effectief de klant wil deleten?
             //verwijdert de klant uit lijst klanten en verwijdert zijn/haar aankopen uit lijst aankopen
             int index = lstKlantenlijst.SelectedIndex;
             if (index == -1)
@@ -276,11 +264,10 @@ namespace Schoenwinkel.Wpf
             string achternaam = verwijderKlant.Achternaam;
             List<int> teDeletenLocaties = new List<int>();
 
-            if(MessageBox.Show($"Wilt u {achternaam} {voornaam} permanent verwijderen?" + Environment.NewLine + "Alsook de door deze klant aangekochte schoenen zullen verwijderd worden.", "Bevestig verwijdering klant", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+            if(MessageBox.Show($"Wilt u {achternaam} {voornaam} permanent verwijderen?" + Environment.NewLine + "Alsook de door deze klant aangekochte schoenen zullen verwijderd worden." + Environment.NewLine + "Echter blijven de aankopen wel in het winkelregister staan.", "Bevestig verwijdering klant", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
             {
                 return; //bij het induwen van de NO knop zal de methode hier afgebroken worden
             }
-
             if (aankopen.Count != 0)
             {
                 for (int i = 0; i >= aankopen.Count; i++) //zoekt alle aankopen door de gebruiker
@@ -304,6 +291,11 @@ namespace Schoenwinkel.Wpf
             ClearCostumerLabels();
             ClearCostumerStatistics();
             UpdateStoreStatistics();
+        }
+        //all this just to highlight a textbox :(
+        private void txtNieuweSchoenmaat_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            txtNieuweSchoenmaat.SelectAll();
         }
     }
 }
